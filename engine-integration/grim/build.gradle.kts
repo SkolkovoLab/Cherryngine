d2982366b6cafe01777cdcd18352eb0ee91a2227
MelonHell <MelonHell@ya.ru>
2025-12-13 02:35:31 +0300

    bump grim packetevents

--- engine-integration/grim/build.gradle.kts
+++ engine-integration/grim/build.gradle.kts
@@ -8,8 +8,8 @@ dependencies {
 
     api("ac.grim.grimac:common:2.3.73-c2c044f+lite-no_relocate")
     api("ac.grim.grimac:GrimAPI:1.2.0.0")
-    api("com.github.retrooper:packetevents-api:2.10.2+3684f01-SNAPSHOT")
-    api("com.github.retrooper:packetevents-netty-common:2.10.2+3684f01-SNAPSHOT")
+    api("com.github.retrooper:packetevents-api:2.11.1+af5bbc0-SNAPSHOT")
+    api("com.github.retrooper:packetevents-netty-common:2.11.1+af5bbc0-SNAPSHOT")
 
     runtimeOnly("org.xerial:sqlite-jdbc:3.51.0.0")
 }
