48557ca72d4c746df35d5aaa9e62aded2e3ca7c0
MelonHell <MelonHell@ya.ru>
2026-04-19 16:09:25 +0300

    fleks one shot components

--- build-logic/src/main/kotlin/cherryngine-java.gradle.kts
+++ build-logic/src/main/kotlin/cherryngine-java.gradle.kts
@@ -11,6 +11,7 @@ repositories {
     google() // Compose-Multiplatform
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
     maven("https://libraries.minecraft.net") // Brigadier
     maven("https://repo.cherry.pizza/repository/maven-public/")
     maven("https://repo.viaversion.com/")
--- engine-ecs/src/main/kotlin/ru/cherryngine/engine/ecs/events/LastPlayerPositionEvent.kt
+++ engine-ecs/src/main/kotlin/ru/cherryngine/engine/ecs/events/LastPlayerPositionEvent.kt
@@ -1,13 +1,14 @@
 package ru.cherryngine.engine.ecs.events
 
 import com.github.quillraven.fleks.ComponentType
+import ru.cherryngine.engine.ecs.EcsComponent
 import ru.cherryngine.lib.math.Vec3D
 import ru.cherryngine.lib.math.YawPitch
 
 data class LastPlayerPositionEvent(
     val position: Vec3D = Vec3D.ZERO,
     val yawPitch: YawPitch = YawPitch.ZERO,
-) : EcsEvent<LastPlayerPositionEvent> {
+) : EcsComponent<LastPlayerPositionEvent> {
     override fun type() = LastPlayerPositionEvent
 
     companion object : ComponentType<LastPlayerPositionEvent>()
