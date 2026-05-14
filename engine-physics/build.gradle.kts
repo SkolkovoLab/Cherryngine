f033b5b7afa3803d3527a11d53f936ca3772d7ea
MelonHell <MelonHell@ya.ru>
2026-05-14 03:09:33 +0300

    jolt-jni 4.3.0

--- engine-physics/build.gradle.kts
+++ engine-physics/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 dependencies {
     api(project(":engine-core"))
 
-    implementation("com.github.stephengold:jolt-jni-Windows64:3.5.0")
-    runtimeOnly("com.github.stephengold:jolt-jni-Linux64:3.5.0:ReleaseSp")
-    runtimeOnly("com.github.stephengold:jolt-jni-Windows64:3.5.0:ReleaseSp")
+    implementation("com.github.stephengold:jolt-jni-Windows64:4.3.0")
+    runtimeOnly("com.github.stephengold:jolt-jni-Linux64:4.3.0:ReleaseSp")
+    runtimeOnly("com.github.stephengold:jolt-jni-Windows64:4.3.0:ReleaseSp")
     implementation("io.github.electrostat-lab:snaploader:1.1.1-stable")
     runtimeOnly("com.github.oshi:oshi-core:6.9.1")
 
